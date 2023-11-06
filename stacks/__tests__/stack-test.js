e721741a52c7115bc0c4909a147e0eef56a81ea9
Matt Levy <milevy1@gmail.com>
2023-11-06 15:39:19 -0600

    Implements Stack using Array.prototype

--- stacks/__tests__/stack-test.js
+++ stacks/__tests__/stack-test.js
@@ -1,19 +1,66 @@
-const Stack = require('../lib/stack');
+const Stack = require('../lib/stack')
 
-test('it pushes', () => {
-  const subject = new Stack();
-  subject.push('A')
-  subject.push('B')
-  subject.push('C')
-  expect(subject.count).toEqual(3);
-});
+let subject
+beforeEach(() => {
+  subject = new Stack()
+})
 
-test('it pops', () => {
-  const subject = new Stack();
-  subject.push('A')
-  subject.push('B')
-  subject.push('C')
-  expect(subject.pop).toEqual('A');
-  expect(subject.pop).toEqual('B');
-  expect(subject.pop).toEqual('C');
-});
+describe('count', () => {
+  it('returns the number of elements on the stack', () => {
+    expect(subject.count).toEqual(0)
+
+    subject.push('A')
+    expect(subject.count).toEqual(1)
+  })
+})
+
+describe('push', () => {
+  it('adds an element to the top of the stack', () => {
+    subject.push('A')
+    subject.push('B')
+    subject.push('C')
+    expect(subject.count).toEqual(3)
+  })
+})
+
+describe('pop', () => {
+  it('removes and returns the top element of the stack', () => {
+    subject.push('A')
+    subject.push('B')
+    subject.push('C')
+
+    expect(subject.pop()).toEqual('C')
+    expect(subject.count).toEqual(2)
+
+    expect(subject.pop()).toEqual('B')
+    expect(subject.count).toEqual(1)
+
+    expect(subject.pop()).toEqual('A')
+    expect(subject.count).toEqual(0)
+  })
+})
+
+
+describe('peek', () => {
+  it('returns the value of the top element without removing it', () => {
+    subject.push('A')
+    subject.push('B')
+
+    expect(subject.peek).toEqual('B')
+    expect(subject.count).toEqual(2)
+  })
+})
+
+describe('empty', () => {
+  it('returns a true if the stack is empty', () => {
+    expect(subject.empty).toEqual(true)
+  })
+
+  it('returns false if the stack is not empty', () => {
+    subject.push('A')
+    expect(subject.empty).toEqual(false)
+
+    subject.pop()
+    expect(subject.empty).toEqual(true)
+  })
+})
--- stacks/lib/stack.js
+++ stacks/lib/stack.js
@@ -1,5 +1,25 @@
 class Stack {
+  data = []
+  
+  push(value) {
+    this.data.push(value)
+  }
+  
+  pop() {
+    return this.data.pop()
+  }
 
+  get count() {
+    return this.data.length
+  }
+
+  get peek() {
+    return this.data.at(-1)
+  }
+
+  get empty() {
+    return this.count === 0
+  }
 }
   
-module.exports = Stack;
+module.exports = Stack
